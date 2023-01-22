b579733cd39e7edd2a29a0e8a90ccf4da74b3264
dmitry <piervov00@gmail.com>
2023-01-22 18:07:47 +0300

    add status bar

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.mapapp
 
-import android.R
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.NetworkInfo.DetailedState
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
+import android.os.BatteryManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mapapp.databinding.ActivityMainBinding
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import org.xmlpull.v1.XmlPullParser
 import java.util.*
 
 
@@ -19,17 +25,35 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    //Battery level
+    private val mBatInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(ctxt: Context?, intent: Intent) {
+            val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            val batteryPct = level * 100 / scale.toFloat()
+            binding.tvBattery.text = "$batteryPct%"
+        }
+    }
+
+    private fun getWifiName(): String? {
+        val mWifiManager = (this.getSystemService(WIFI_SERVICE) as WifiManager)
+        val info = mWifiManager.connectionInfo
+        Log.d("develop", "info: $info")
+        return info.ssid
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapKitFactory.setApiKey("da2a62fc-d17b-4ff5-9690-480e7e998219")
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         setFullScreen()
         initView()
+
+        //receiver for battery level
+        this.registerReceiver(this.mBatInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        binding.tvNetwork.text = getWifiName()
 
     }
 
@@ -63,7 +87,6 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
-
 
 
 //        Log.d("develop", "${}")
